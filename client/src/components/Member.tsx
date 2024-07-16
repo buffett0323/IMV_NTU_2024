230c048d96d6c11b7b96088d91eabd9948ebccf9
buffett0323 <philip910323@gmail.com>
2024-07-16 23:25:28 +0800

    Member Adding

--- client/src/components/Member.tsx
+++ client/src/components/Member.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/Member.css';
 
 const Member: React.FC = () => {
   return (
     <section className="member">
       <h2>會員資訊</h2>
       <p>這裡是會員資訊頁面。</p>
+      <div className="auth-buttons">
+        <Link to="/login" className="btn">Login</Link>
+        <Link to="/signup" className="btn">Signup</Link>
+      </div>
     </section>
   );
 }
